e5e19dbff8a13c9123475f87bd7770b77369d387
pallaviferrao <pallaviferrao2004@gmail.com>
2022-03-19 17:20:32 +0530

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,87 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+const port = process.env.PORT || 3000;
+module.exports = {
+  entry: "/src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  resolve: {
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      //   {
+      //     test: /\.(jpg|png)$/,
+      //     use: {
+      //       loader: "url-loader",
+      //     },
+      //   },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      //   {
+      //     test: /\.css$/,
+      //     use: [
+      //       {
+      //         loader: "style-loader",
+      //       },
+      //       {
+      //         loader: "css-loader",
+      //         options: {
+      //           modules: true,
+      //           localsConvention: "camelCase",
+      //           sourceMap: true,
+      //         },
+      //       },
+      //     ],
+      //   },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: port,
+    hot: true,
+    historyApiFallback: true,
+    open: true,
+  },
+};
