26e0bbe5e46e2eafc199d9fa8f41bc38c8e8bb85
pallaviferrao <pallaviferrao2004@gmail.com>
2022-03-19 21:08:10 +0530

    adding deployment changes

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import "./home.css";
-import Portfolio from "./Portfolio.jsx";
+// import Portfolio from "./Portfolio.jsx";
 import Games from "../Games/Games.jsx";
-import Intro from "./Intro.jsx";
+// import Intro from "./Intro.jsx";
+import Loading from "../Loading";
+const AsyncPortfolio = importedComponent(
+  () => import(/* webpackChunkName:'DynamicPage' */ "./Portfolio.jsx"),
+  {
+    LoadingComponent: Loading,
+  }
+);
+const AsyncIntro = importedComponent(
+  () => import(/* webpackChunkName:'DynamicPage' */ "./Intro.jsx"),
+  {
+    LoadingComponent: Loading,
+  }
+);
 function Home() {
-  const [page, setPage] = useState(<Intro />);
+  const [page, setPage] = useState(<AsyncIntro />);
   setTimeout(() => {
-    setPage(<Portfolio />);
+    setPage(<AsyncPortfolio />);
   }, 5000);
   //return page;
   return (
---
+++ webpack.config.production.js
@@ -0,0 +1,104 @@
+const path = require("path");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "production",
+  entry: "/src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+  },
+  resolve: {
+    alias: {
+      "react-dom": "@hot-loader/react-dom",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      //   {
+      //     test: /\.(jpg|png)$/,
+      //     use: {
+      //       loader: "url-loader",
+      //     },
+      //   },
+      {
+        test: /\.(gif|png|jpe?g)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "assets/images/",
+            },
+          },
+        ],
+      },
+
+      {
+        test: /\.html$/,
+        use: ["html-loader"],
+      },
+      //   {
+      //     test: /\.css$/,
+      //     use: [
+      //       {
+      //         loader: "style-loader",
+      //       },
+      //       {
+      //         loader: "css-loader",
+      //         options: {
+      //           modules: true,
+      //           localsConvention: "camelCase",
+      //           sourceMap: true,
+      //         },
+      //       },
+      //     ],
+      //   },
+    ],
+  },
+  devtool: "source-map",
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: "styles",
+          test: /\.css$/,
+          chunks: "all",
+          enforce: true,
+        },
+        vendor: {
+          chunks: "initial",
+          test: "vendor",
+          name: "vendor",
+          enforce: true,
+        },
+      },
+    },
+  },
+  performance: {
+    hints: false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+    }),
+  ],
+};
