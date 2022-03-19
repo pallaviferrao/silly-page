7d6f54711f3a2abd68fa50c362f29edd8e9afbf5
pallaviferrao <pallaviferrao2004@gmail.com>
2022-03-19 22:19:51 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,26 +1,13 @@
 import React, { useState, useEffect } from "react";
 import { Routes, BrowserRouter as Router, Route } from "react-router-dom";
 import "./home.css";
-// import Portfolio from "./Portfolio.jsx";
+import Portfolio from "./Portfolio.jsx";
 import Games from "../Games/Games.jsx";
-// import Intro from "./Intro.jsx";
-import Loading from "../Loading";
-const AsyncPortfolio = importedComponent(
-  () => import(/* webpackChunkName:'DynamicPage' */ "./Portfolio.jsx"),
-  {
-    LoadingComponent: Loading,
-  }
-);
-const AsyncIntro = importedComponent(
-  () => import(/* webpackChunkName:'DynamicPage' */ "./Intro.jsx"),
-  {
-    LoadingComponent: Loading,
-  }
-);
+import Intro from "./Intro.jsx";
 function Home() {
-  const [page, setPage] = useState(<AsyncIntro />);
+  const [page, setPage] = useState(<Intro />);
   setTimeout(() => {
-    setPage(<AsyncPortfolio />);
+    setPage(<Portfolio />);
   }, 5000);
   //return page;
   return (
