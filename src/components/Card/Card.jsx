91d107fd394abc25beff1b6e408c11452f890cf0
pallaviferrao <pallaviferrao2004@gmail.com>
2022-05-06 23:10:27 +0530

    login

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import "./card.css";
+import picture from "../../assets/images/cardimage.jpg";
 const Card = (props) => {
   return (
     <div className="lolo" onClick={() => props.handleClick()}>
+      <div className="cardImage">
+        <img src={picture}></img>
+      </div>
       <h1 className="title">{props.title}</h1>
-      {/* <div className="image">
-        <img src={props.title}></img>
-      </div> */}
-      {/* <img src={props.image} width="10px"></img> */}
+
       <div className="description">{props.description}</div>
-      {/* <button className="cardButton" onClick={props.handleClick}>
-        {props.buttonTitle}
-      </button> */}
     </div>
   );
 };
