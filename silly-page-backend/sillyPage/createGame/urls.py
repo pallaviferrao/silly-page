8b7dfc945046ae2cdb23b0bccef179c6fe7e8435
pallaviferrao <pallaviferrao2004@gmail.com>
2023-10-14 14:12:34 +0530

    Remove unnecessary code

--- silly-page-backend/sillyPage/createGame/urls.py
+++
@@ -1,7 +0,0 @@
-import imp
-from django.urls import path
-from createGame import views
-
-urlpatterns = [ 
-    path("", views.createGame, name="createGame")
-]
--- silly-page-backend/sillyPage/createGame/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse;
-
-# Create your views here.
-
-def createGame(request):
-    return HttpResponse("Hello")
